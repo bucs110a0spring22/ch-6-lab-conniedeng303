a60531b1c51ac13a5d8e506d585044c8798dab00
Connie Deng Deng <98533897+conniedeng303@users.noreply.github.com>
2022-03-27 22:18:46 +0000

    justbeginningcode

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-
 def seq3np1(n):
-    """
-        Print the 3n+1 sequence from n, terminating when it reaches 1.
-        args: n (int) starting value for 3n+1 sequence
-        return: None
-    """
-    while(n != 1):
-        print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
+  count = 0
+  while n != 1:
+    print(n)
+    if(n % 2) == 0:        # n is even
+      n = n // 2
+      count += 1
+    else:                 # n is odd
+      n = n * 3 + 1
+      print(n)  
+      count += 1 # the last print is 1
+  print("the count should be" ,count)
+  
 
 def main():
-	seq3np1(3)
+	seq3np1(10)
 main()
